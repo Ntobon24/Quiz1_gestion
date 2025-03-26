6d4bd06c512e5bc3f42eab403cb32685379f7908
Nicolas Tobon <nicolastobon242>
2025-03-26 16:14:05 -0500

    Cambios en los textos

--- src/App.jsx
+++ src/App.jsx
@@ -16,17 +16,14 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>QUIZ 1 GESTION DE LA CONFIGURACION</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        NO es mucho pero es trabajo honesto
       </p>
     </>
   )
